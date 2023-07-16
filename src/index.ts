2ade39f915a772c48c73849dceb52c75e9a168e5
Muhammad Awais <muhammad.awais.20024@gmail.com>
2023-07-16 14:15:40 +0500

    Start express server.

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,11 @@
-require("dotenv").config();
+import express from "express";
+import dotenv from "dotenv";
 
-console.log("Hello Eclinic");
+dotenv.config();
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
