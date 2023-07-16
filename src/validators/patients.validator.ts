0624b2c4c0e79e64969f582d1f4c04a168920174
Muhammad Awais <muhammad.awais.20024@gmail.com>
2023-07-16 21:22:50 +0500

    Installed joi and added create validator for patient.

---
+++ src/validators/patients.validator.ts
@@ -0,0 +1,16 @@
+import Patient from "../types/patient";
+import Joi from "joi";
+
+export default {
+  create: function (patient: Patient) {
+    const schema = Joi.object({
+      fist_name: Joi.string().required(),
+      last_name: Joi.string().required(),
+      email: Joi.string().email().required(),
+      phone: Joi.string().required(),
+      birthdate: Joi.date().required(),
+      password: Joi.string().required(),
+    });
+    return schema.validate(patient);
+  },
+};
