3f89c9a7ed84b659775d6cb7ee26b542a4ead4c4
Muhammad Awais <muhammad.awais.20024@gmail.com>
2023-07-16 13:41:46 +0500

    Project boilerplate.

---
+++ src/constants/index.ts
@@ -0,0 +1,32 @@
+module.exports = {
+  // codes
+  SUCCESS: 200,
+  CREATED: 201,
+  BAD_REQUEST: 400,
+  UNAUTHORIZED: 401,
+  NOT_FOUND: 404,
+  INTERNAL_SERVER_ERROR: 500,
+
+  // messages
+  SUCCESS_MSG: "Success",
+  CREATED_MSG: "Created",
+  BAD_REQUEST_MSG: "Bad Request",
+  UNAUTHORIZED_MSG: "Unauthorized",
+  NOT_FOUND_MSG: "Not Found",
+  INTERNAL_SERVER_ERROR_MSG: "Internal Server Error",
+
+  // error messages
+  INVALID_EMAIL: "Invalid email",
+  INVALID_PASSWORD: "Invalid password",
+
+  // user
+  USER_NOT_FOUND: "User not found",
+  USER_ALREADY_EXISTS: "User already exists",
+  USER_CREATED: "User created",
+  USER_DELETED: "User deleted",
+
+  // auth
+  AUTH_SUCCESS: "Authentication successful",
+  AUTH_FAILED: "Authentication failed",
+  AUTH_REQUIRED: "Authentication required",
+};
