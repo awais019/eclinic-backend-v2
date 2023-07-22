04d71640c94eadeaff980718b1e39e0edbb51c85
Muhammad Awais <muhammad.awais.20024@gmail.com>
2023-07-22 20:02:45 +0500

    Added route for file uploads.

---
+++ src/routes/uploads.routes.ts
@@ -0,0 +1,9 @@
+import express from "express";
+
+const router = express.Router();
+
+router.post("/", async (req, res) => {
+  res.send();
+});
+
+export default router;
--- src/startup/routes.ts
+++ src/startup/routes.ts
@@ -2,9 +2,11 @@ import { Express } from "express";
 import index from "../routes/index.routes";
 import patients from "../routes/patients.routes";
 import doctors from "../routes/doctors.routes";
+import uploads from "../routes/uploads.routes";
 
 export default function (app: Express) {
   app.use("/api/", index);
   app.use("/api/patients", patients);
   app.use("/api/doctors", doctors);
+  app.use("/api/upload", uploads);
 }
