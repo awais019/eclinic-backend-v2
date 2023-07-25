92c53b0ed53a8a05d8e30fc881f35a77bc8245dd
Muhammad Awais <muhammad.awais.20024@gmail.com>
2023-07-25 18:06:47 +0500

    Changed upload dir path.

--- src/helpers/upload.ts
+++ src/helpers/upload.ts
@@ -6,7 +6,7 @@ export default {
   uploadFile: async (file: UploadedFile, folder: string) => {
     const newName = `${randomUUID()}${path.extname(file.name)}`;
     const uploadPath = path.resolve(
-      `./src/public/uploads/${folder}/${newName}`
+      `./public/uploads/${folder}/${newName}`
     );
 
     await file.mv(uploadPath);
