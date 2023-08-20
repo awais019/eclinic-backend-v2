e130d8563f76d17c1e046cd8d04f77222a6f95d4
Muhammad Awais <muhammad.awais.20024@gmail.com>
2023-08-20 15:29:35 +0500

    Changed get reviews end point

--- src/routes/doctors.routes.ts
+++ src/routes/doctors.routes.ts
@@ -41,7 +41,7 @@ router.get("/", trycatchMiddleware(doctors.getDoctors));
 
 router.get("/:id", trycatchMiddleware(doctors.getDoctor));
 
-router.get("/reviews/:id", trycatchMiddleware(doctors.getReviews));
+router.get("/:id/reviews", trycatchMiddleware(doctors.getReviews));
 
 router.get("/specializations", trycatchMiddleware(doctors.getSpecializations));
 
