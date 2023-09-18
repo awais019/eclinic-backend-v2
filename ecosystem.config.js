f6afe97ecc3b02c6e538cd38d54088c8dd0da544
Muhammad Awais <muhammad.awais.20024@gmail.com>
2023-09-18 21:44:40 +0500

    changed port

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
     {
       name: "eclinic-backend",
       exec_mode: "cluster",
-      port: 3000,
+      port: 4000,
       instances: "1",
       script: "./dist/index.js",
     },
