9ecf23eaacd019abef91c3c3d5b75f357be0a12a
Muhammad Awais <muhammad.awais.20024@gmail.com>
2023-10-02 19:51:19 +0500

    Added sending notification for a new conversation

--- src/startup/socket.ts
+++ src/startup/socket.ts
@@ -3,6 +3,8 @@ import messageService from "../services/message.service";
 import logger from "./logger";
 
 export default function socket(server: any) {
+  const clients = new Map();
+
   const io = new Server(server, {
     cors: {
       origin: "*",
@@ -12,6 +14,10 @@ export default function socket(server: any) {
 
   io.on("connection", (socket) => {
     logger.info("User connected to socket");
+
+    socket.on("set-user", (id: string) => {
+      clients.set(id, socket.id);
+    });
 
     socket.on("join", (conversationId: string) => {
       socket.join(conversationId);
@@ -27,6 +33,26 @@ export default function socket(server: any) {
         receiver: string;
       }) => {
         const message = await messageService.createMessage(data);
+
+        const socketsInRoom = io.sockets.adapter.rooms.get(data.conversationId);
+
+        if (socketsInRoom && socketsInRoom.size == 1) {
+          console.log("sending notification");
+          const conversations = await messageService.getConversations(
+            data.receiver
+          );
+          const receiverSocketId = clients.get(data.receiver);
+          io.to(receiverSocketId).emit(
+            "new-message",
+            conversations.map((conversation) => {
+              return {
+                id: conversation.id,
+                Participant: conversation.Participant[0].User,
+                Message: conversation.Message[0],
+              };
+            })
+          );
+        }
         io.to(data.conversationId).emit("message", message);
       }
     );
