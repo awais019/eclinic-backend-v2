121a1f20d05ac5b6d895881d2ac622805bee6906
Muhammad Awais <muhammad.awais.20024@gmail.com>
2024-02-06 12:34:25 +0500

    Removed unused files and directories and updated dependencies.

--- dist/helpers/ejs.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const ejs_1 = require("ejs");
-exports.default = {
-    renderHTMLFile: (template, templateData) => {
-        return (0, ejs_1.renderFile)(`views/${template}.ejs`, templateData);
-    },
-};
-//# sourceMappingURL=ejs.js.map
